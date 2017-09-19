366de1e1ff320f11cfc106cf69c7fb9f8478f377
ummdorian <dorian@adworkshop.com>
2017-09-18 20:59:49 -0400

    Token Save Button

--- Pantheon.java
+++ Pantheon.java
@@ -5,7 +5,11 @@ import javax.swing.*;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Properties;       
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.awt.*;
+import java.awt.event.*;  
 
 public class Pantheon {
 	
@@ -15,13 +19,16 @@ public class Pantheon {
 	protected JLabel label;
 	protected JTextField tokenTextField;
 	
+	protected InputStream input = null;
+	protected Properties prop = new Properties();
+	
 	
 	/**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
      * event-dispatching thread.
      */
-    public void createAndShowGUI() {
+    public void createAndShowGUI(Pantheon pantheon) {
 	
         // Create Window
         frame = new JFrame("Pantheon Utilities");
@@ -30,9 +37,11 @@ public class Pantheon {
 		//----------------------------------------------------
 		// Create Container, Label and Input for Access Token
 		//----------------------------------------------------
+		
 		// Panel
 		tokenTextFieldPanel = new JPanel();
 		frame.getContentPane().add(tokenTextFieldPanel);
+		
 		// Label
 		label = new JLabel("Pantheon Access Token");
 		tokenTextFieldPanel.add(label);
@@ -40,12 +49,49 @@ public class Pantheon {
 		// Text Field
 		tokenTextField = new JTextField(20);
 		tokenTextFieldPanel.add(tokenTextField);
+		// Load text field value
+		tokenTextField.setText(loadConfig("accessToken"));  
+		
+		// Button
+		JButton tokenTextFieldSaveButton = new JButton("Save/Update Token");
+		tokenTextFieldPanel.add(tokenTextFieldSaveButton);
+		// Button Submit Function
+		tokenTextFieldSaveButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				// Config Save
+				Pantheon pantheonTemp = new Pantheon();
+				pantheonTemp.saveConfigValue("accessToken",tokenTextField.getText());
+			}
+		});
 
         //Display the window.
         frame.pack();
         frame.setVisible(true);
 		
     }
+	
+	
+	public String loadConfig(String key){
+		try {
+			input = new FileInputStream("config.properties");
+			
+			// load a properties file
+			prop.load(input);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return prop.getProperty(key);
+	}
+	
 	
 	/**
 	* See http://www.mkyong.com/java/java-properties-file-examples/
@@ -74,14 +120,19 @@ public class Pantheon {
 		}
 	}
 
+	
     public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
+        
+		//Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+				// Create a new instance of this class so we can call non static functions
 				Pantheon pantheon = new Pantheon();
-                pantheon.createAndShowGUI();
+				// Create and show GUI
+                pantheon.createAndShowGUI(pantheon);
             }
         });
+		
     }
 }
